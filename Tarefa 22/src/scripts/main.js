01104b8f68f8cbaf707bf426baca9fa5ab6a5984
TamM619 <mateusenrique123@gmail.com>
2025-06-24 13:16:51 -0300

    Modulo 22 concluido e a tarefa, git organizado.

---
+++ Tarefa 22/src/scripts/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+const dataDoEvento = new Date("Dec 24, 2025 09:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    //digitalizando os ms
+    const diaMs = 1000 * 60 * 60 * 24;
+    const horaMs = 1000 * 60 * 60;
+    const minutoMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / (diaMs));
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % (diaMs)) / (horaMs));
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % (horaMs)) / (minutoMs));
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % (minutoMs)) / (1000));
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+
+    document.getElementById('contador').innerHTML = `será em ${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'já passou.'
+    }
+}, 1000)
